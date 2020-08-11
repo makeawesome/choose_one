5bd7f79afea98a854c90a0090040c34b5c516648
MGH <kirin@ParkJung-hyunui-MacBook-Pro.local>
2020-08-11 23:33:15 +0900

    checked boxes value control

--- src/components/TopCategories.vue
+++ src/components/TopCategories.vue
@@ -1,14 +1,15 @@
 <template>
     <div>
         <span class="title">나는 오늘...</span>
-        <div id="categories">
+        <div id="categories" >
             <template v-for="category in categories">
-                <input type="checkbox" :key="category.id" :value="category.id" :id="category.id" />
+                <input type="checkbox" :key="category.id + category.name" :value="category.id" :id="category.id" v-model="checkedItems">
                 <label :for="category.id" :key="category.id">{{category.name}}</label>
             </template>
         </div>
 
         <div id="buttons">
+            <button type="button" @click="all()">전체</button>
             <button type="button" @click="init()">초기화</button>
             <button type="button" @click="exclude()">...빼고</button>
             <button type="button" @click="include()">...중에서</button>
@@ -18,29 +19,47 @@
 <script>
 export default {
     data() {
-        let categories = [];
-        let rootCategories = this.$store.state.rootCategories;
-
-        for(let categoryKey in rootCategories) {
-            categories.push({
+        let rootCategories = this.$store.state.rootCategories || [];
+        let categories = Object.keys(rootCategories).map(categoryKey => {
+            return {
                 id: categoryKey,
                 name: rootCategories[categoryKey].name
-            })
-        }
+            };
+        });
 
         return {
-            categories
+            categories,
+            checkedItems: [],
+            uncheckedItems: Object.keys(rootCategories)
         };
     },
-    methods: {
-        init() {
 
+    // 아무것도 선택 안했을 때 -> ...중에서, ...빼고 비활성화
+    // 모두 선택 했을 때 -> ...빼고 비활성화
+    watch: {
+
+    },
+
+    methods: {
+        all() {
+            this.data.checkedItems = Object.keys(this.data.categories);
+        },
+        init() {
+            this.data.checkedItems = [];
         },
         exclude() {
+            if(this.data.checkedItems.length === 0) {
+                return;
+            }
 
+            console.log(this.data.uncheckedItems);
         },
         include() {
+            if(this.data.checkedItems.length === 0) {
+                return;
+            }
 
+            console.log(this.data.checkedItems);
         }
     }
 }
