44b3b3dc4d893da732c5a5957a39a94666f2ee86
Patrick Min <makereallyawesome@gmail.com>
2020-09-04 16:57:16 +0900

    returning all child-categories that included parents id array

--- src/models/CategoryModel.mjs
+++ src/models/CategoryModel.mjs
@@ -1,9 +1,11 @@
 let names = ['', '한식', '중식', '양식', '일식', '태국식', '멕시칸'];
 
+const parentsNum = 6;
+
 const topCategories = () => {
     let categories = [];
 
-    for (let i = 1; i <= 6; i++) {
+    for (let i = 1; i <= parentsNum; i++) {
         categories.push({
             id: i + '',
             name: names[i]
@@ -13,24 +15,45 @@ const topCategories = () => {
     return categories;
 };
 
-const childCategories = parentId => {
+const allChildCategoriesData = () => {
     let categories = [];
-    for (let i = 1; i <= 6; i++) {
+    for (let i = 1; i <= parentsNum; i++) {
         let data = [];
-        for (let j = 1; j <= 6; j++) {
+        for (let j = 1; j <= parentsNum; j++) {
             data.push({
                 id: j + '',
-                name: names[j] + j
+                name: names[i] + j,
+                img: '/dist/giphy.gif?f49537d85dcdf225e9047a833d2b3e7f'
             });
         }
 
         categories.push({
-            parentId: i,
+            parentId: i + '',
             data
         });
     }
 
-    return categories.filter(cat => cat.parentId === parentId);
+    return categories;
+}
+
+const childCategories = parentIds => {
+    let childCategories = allChildCategoriesData();
+
+    if (!parentIds) {
+        return [];
+    }
+
+    let chosenChild = [];
+    parentIds.forEach(parentId => {
+        let child = childCategories.find(child => child.parentId === parentId);
+        if (child) {
+            (child.data || []).forEach(data => {
+                chosenChild.push(data);
+            });
+        }
+    })
+
+    return chosenChild;
 };
 
 export { topCategories, childCategories };
